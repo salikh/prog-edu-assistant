94259ed4fcdd8a445bc1c162627532528c103e29
Salikh Zakirov <salikh@google.com>
2019-08-05 23:35:41 +0900

    use 4 workers on deployment

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -16,6 +16,6 @@ set -e
   deploy/{certs,docker-compose.yml,secret.env,service-account.json} \
   $GCE_HOST:
 
-@execute ssh $GCE_HOST "mkdir -p logs && export GCP_PROJECT=${GCP_PROJECT?} && docker ps -q | xargs -n1 docker kill; cat service-account.json | docker login -u _json_key --password-stdin https://asia.gcr.io && docker pull asia.gcr.io/${GCP_PROJECT?}/worker && docker pull asia.gcr.io/${GCP_PROJECT?}/server && docker run -d --rm -v /var/run/docker.sock:/var/run/docker.sock -v \$PWD:\$PWD -w=\$PWD --entrypoint=sh docker/compose:1.24.0 -c 'cat service-account.json | docker login -u _json_key --password-stdin https://asia.gcr.io && export GCP_PROJECT=${GCP_PROJECT?} && docker-compose up --scale worker=2'"
+@execute ssh $GCE_HOST "mkdir -p logs && export GCP_PROJECT=${GCP_PROJECT?} && docker ps -q | xargs -n1 docker kill; cat service-account.json | docker login -u _json_key --password-stdin https://asia.gcr.io && docker pull asia.gcr.io/${GCP_PROJECT?}/worker && docker pull asia.gcr.io/${GCP_PROJECT?}/server && docker run -d --rm -v /var/run/docker.sock:/var/run/docker.sock -v \$PWD:\$PWD -w=\$PWD --entrypoint=sh docker/compose:1.24.0 -c 'cat service-account.json | docker login -u _json_key --password-stdin https://asia.gcr.io && export GCP_PROJECT=${GCP_PROJECT?} && docker-compose up --scale worker=4'"
 
 echo OK
