f7fd72c7014174379800a5bc9b16087039a87deb
Salikh Zakirov <salikh@google.com>
2019-08-05 11:13:12 +0900

    start-local-docker.sh

---
+++ start-local-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# A convenience script to build and start a local docker instance with the
+# all the containers that comprise the autochecker service (worker, server,
+# rabbitmq).
+
+set -ex
+cd "$(dirname "$0")"
+bazel test ...
+cd docker
+./build.sh
+docker-compose up
